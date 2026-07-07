4908d42362398a2e61e632062b42affbe07786a4
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-07 23:50:28 +0530

    Add Split Array Largest Sum solution class

---
+++ Binary Search/Split Array largest Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int countSplits(vector<int>& nums, int maxSum){
+        int splits = 1;
+        long long currentSum = 0;
+        for(int i =0; i<nums.size(); i++){
+            if(currentSum + nums[i] <= maxSum){
+                currentSum += nums[i];
+            }
+            else{
+                splits += 1;
+                currentSum = nums[i];
+            }
+        }
+        return splits;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(),0);
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int splits = countSplits(nums, mid);
+            if(splits > k) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+}; 
