402de0d0a5a84460c9fcf1a4fb91550eb020437f
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-09 23:56:19 +0530

    Add cycle detection using Tortoise and Hare method
    Implemented the Tortoise and Hare algorithm to detect a cycle in a linked list.

--- Linked List/Linked List Cycle II.cpp
+++ Linked List/Linked List Cycle II.cpp
@@ -14,3 +14,26 @@ public:
         return NULL;
     }
 };
+
+// Optimal[Tortoise hare method]
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head->next) return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
+                fast = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return fast;
+            }
+        } 
+        return NULL;
+    }
+};
