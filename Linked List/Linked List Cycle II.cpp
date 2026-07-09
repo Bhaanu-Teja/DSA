f9c4e206e60a57cc8853f79603acb3d6f3e24364
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-09 23:55:13 +0530

    Add cycle detection in linked list using hashing
    Implement brute force method to detect cycle in linked list using hashing.

---
+++ Linked List/Linked List Cycle II.cpp
@@ -0,0 +1,16 @@
+// Brute Force [Hashing]
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        map<ListNode*, int> mpp;
+        ListNode* temp = head;
+        while(temp != NULL){
+            if(mpp.find(temp) != mpp.end()){
+                return temp;
+            }
+            mpp[temp] = 1;
+            temp = temp->next;
+        }
+        return NULL;
+    }
+};
