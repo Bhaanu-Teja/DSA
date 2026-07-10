7ea118b74e78587248ef2b416ac15ae00cc5a1a9
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-10 17:16:50 +0530

    Implement optimal palindrome check for linked list
    Added an optimal solution for checking if a linked list is a palindrome by reversing the second half of the list.

--- Linked List/Palindrome Linked List.cpp
+++ Linked List/Palindrome Linked List.cpp
@@ -1,3 +1,4 @@
+// Brute --> Stack
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -16,6 +17,46 @@ public:
             else{
                 return false;
             }
+        }
+        return true;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+
+    ListNode* reverseLL(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr != NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow= slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newHead = reverseLL(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second!= NULL){
+            if(first->val != second->val){
+                return false;
+            }
+            first = first->next;
+            second = second->next;
         }
         return true;
     }
