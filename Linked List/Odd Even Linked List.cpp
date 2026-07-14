7a2b077b2eb933456b908ccdc013f2c1e0ec8b5a
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-14 09:05:29 +0530

    Add optimal solution for odd-even linked list
    Implemented an optimal solution for rearranging odd and even nodes in a linked list, while retaining the original order.

--- Linked List/Odd Even Linked List.cpp
+++ Linked List/Odd Even Linked List.cpp
@@ -1,13 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Brute
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        vector<int>output;
+        ListNode* temp = head;
+        if(head == NULL || head->next == NULL || head->next->next == NULL) return head;
+        while(temp != NULL){
+            output.push_back(temp->val);
+            if(temp->next == NULL) break;
+            temp= temp->next->next;
+        }
+        temp = head->next;
+        while(temp != NULL){
+            output.push_back(temp->val);
+            if(temp->next == NULL) break;
+            temp= temp->next->next;
+        }
+        temp = head;
+        int i = 0;
+        while(temp != NULL){
+            temp->val = output[i];
+            i++;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
+// Optimal
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
