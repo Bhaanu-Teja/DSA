5f838ec30d2d578256a031d7d0852d1023419519
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-14 19:40:11 +0530

    Implement merge sort for linked list

--- Linked List/Sort List.cpp
+++ Linked List/Sort List.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
@@ -18,3 +19,55 @@ public:
         return head;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    ListNode* merge(ListNode* leftHead, ListNode* rightHead){
+        ListNode dummy(0);
+        ListNode* temp = &dummy;
+        ListNode* l = leftHead;
+        ListNode* r = rightHead;
+        while(l != NULL && r != NULL){
+            if(l->val <= r->val){
+                temp->next = l;
+                l = l->next;
+            }
+            else{
+                temp->next = r;
+                r = r->next;
+            }
+            temp = temp->next;
+        }
+        while(l != NULL){
+            temp->next = l;
+            l = l->next;
+            temp = temp->next;
+        }
+        while(r != NULL){
+            temp->next = r;
+            r = r->next;
+            temp = temp->next;
+        }
+        return dummy.next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* slow = head;
+        ListNode* fast = head; 
+        ListNode* middle = NULL;
+        while(fast != NULL && fast->next != NULL){
+            middle = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }       
+        ListNode* leftHead = head;
+        ListNode* rightHead = middle->next;
+        middle->next = NULL;
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+        return merge(leftHead,rightHead);        
+    }
+};
+
