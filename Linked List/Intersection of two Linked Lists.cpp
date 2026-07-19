643f0e9c923114e8f1a8c8fce81a13ec9f7f78c0
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-19 20:45:10 +0530

    Refactor intersection detection in linked lists
    Refactored the intersection detection method for linked lists by implementing three approaches: brute force, better, and optimal. Each approach is encapsulated in its own class method for clarity and performance comparison.

--- Linked List/Intersection of two Linked Lists.cpp
+++ Linked List/Intersection of two Linked Lists.cpp
@@ -1,3 +1,67 @@
+// Brute
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        map<ListNode*,int> mpp;
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        while(temp1 != NULL){
+            mpp[temp1] = 1;
+            temp1 = temp1->next;
+        }
+        while(temp2 != NULL){
+            if(mpp.count(temp2)){
+                return temp2;
+            }
+            temp2 = temp2->next;
+        }
+        return NULL;
+    }
+};
+
+// Better
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        int count1 = 0;
+        int count2 = 0;
+        while(temp1 != NULL){
+            temp1 = temp1->next;
+            count1++;
+        }
+        while(temp2 != NULL){
+            temp2 = temp2->next;
+            count2++;
+        }
+        int diff = abs(count1 - count2);
+        temp1 = headA;
+        temp2 = headB;
+        if(count1 > count2){
+            while(diff > 0 && temp1 != NULL){
+                temp1 = temp1->next;
+                diff--;
+            }
+        }
+        else{
+            while(diff > 0 && temp2 != NULL){
+                temp2 = temp2->next;
+                diff--;
+            }
+        }
+        while(temp1 != NULL && temp2 != NULL){
+            if(temp1 == temp2){
+                return temp1;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }       
+        return NULL;
+    }
+};
+
+// Optimal
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
