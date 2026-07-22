1056ce374ff11fc80bc1f45e215eaf0576140b51
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-22 19:16:29 +0530

    Implement optimal next smaller element solution
    Added an optimal solution for finding the next smaller elements using a stack.

--- Stack & Queue/Next Smaller Element.cpp
+++ Stack & Queue/Next Smaller Element.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution {
 public:
     vector<int> nextSmallerElements(const vector<int>& arr) {
@@ -13,6 +14,23 @@ public:
                 }
             }
             ans.push_back(nextGreater);
+        }
+        return ans;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    vector<int> nextSmallerElements(const vector<int>& arr) {
+        int n = arr.size();
+        vector<int>ans;
+        stack<int>st;
+        for(int i = n-1; i>= 0; i--){
+            while(!st.empty() && st.top() >= arr[i])
+                st.pop();
+            ans[i] = st.empty() ? -1 : st.top();
+            st.push(arr[i]);
         }
         return ans;
     }
