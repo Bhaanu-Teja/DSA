0e6cc9981d0bd15e524746befd0406fab0f72eb4
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-22 17:21:01 +0530

    Implement nextGreaterElement using stack and map

--- Stack & Queue/Next Greater element - I.cpp
+++ Stack & Queue/Next Greater element - I.cpp
@@ -24,3 +24,31 @@ public:
         return ans;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>nextGreater;
+        stack<int>st;
+        for(int i=0; i<nums2.size();i++){
+            int x = nums2[i];
+            while(!st.empty() && st.top() < x){
+                 nextGreater[st.top()] = x;
+                st.pop();
+            }
+            st.push(x);
+        }
+        while(!st.empty()){
+        nextGreater[st.top()] = -1;
+        st.pop();
+        }
+        vector<int>ans;
+        ans.reserve(nums1.size());
+        for(int i = 0; i<nums1.size(); i++){
+            int x = nums1[i];
+            ans.push_back(nextGreater[x]);
+        }
+        return ans;
+    }
+};
