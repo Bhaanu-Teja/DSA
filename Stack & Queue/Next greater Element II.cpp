8dd9ca938c9e66fa4be70cd89bf6ea778a30abf9
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-22 19:13:07 +0530

    Update Next greater Element II.cpp

--- Stack & Queue/Next greater Element II.cpp
+++ Stack & Queue/Next greater Element II.cpp
@@ -31,3 +31,24 @@ public:
         return ans;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>NGE;
+        stack<int>st;
+        for(int i = 2*n-1; i>= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n])
+                st.pop();
+            if(i < n){
+                if(st.empty()) NGE.push_back(-1);
+                else NGE.push_back(st.top());
+            }
+            st.push(nums[i%n]);
+        }
+        reverse(NGE.begin(), NGE.end());
+        return NGE;
+    }
+};
