785357c870ae6de4241d7a2d68e05ea61b7dd47f
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-25 13:02:45 +0530

    Add optimal solution for trapping rainwater problem

--- Stack & Queue/Trapping Rainwater.cpp
+++ Stack & Queue/Trapping Rainwater.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -19,6 +20,40 @@ public:
             int rightMax = suffix[i];
             if(height[i] < leftMax && height[i] < rightMax){
                 total += min(leftMax, rightMax) - height[i];
+            }
+        }
+        return total;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lMax = 0;
+        int rMax = 0;
+        int total = 0;
+        int l = 0;
+        int r = n-1;
+        while(l < r){
+            if(height[l] <= height[r]){
+                if(lMax > height[l]){
+                    total += lMax - height[l];
+                }
+                else{
+                    lMax = height[l];
+                }
+                l = l+1;
+            }
+            else{
+                if(rMax > height[r]){
+                    total += rMax - height[r];
+                }
+                else{
+                    rMax = height[r];
+                }
+                r= r-1;
             }
         }
         return total;
