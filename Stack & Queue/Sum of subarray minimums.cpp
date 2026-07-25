71f963ef80bec4e48d52649f36b1a4f0f860685a
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-25 22:01:02 +0530

    Implement optimal solution for subarray minimums
    Added an optimal solution for calculating the sum of subarray minimums using a stack.

---
+++ Stack & Queue/Sum of subarray minimums.cpp
@@ -0,0 +1,49 @@
+// Brute
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        long long sum = 0;
+        long long modulo = (1e9 + 7);
+        for(int i = 0; i<n; i++){
+            int mini = arr[i];
+            for(int j = i; j<n; j++){
+                mini = min(mini,arr[j]);
+                sum = (sum + mini) % modulo;
+            }
+        }
+        return sum;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        long long sum = 0;
+        long long mod = 1e9 + 7;
+
+        vector<int> left(n), right(n);
+        stack<int>st;
+
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            left[i] = st.empty() ? (i+1) : (i - st.top());
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            right[i] = st.empty() ? (n-i) : (st.top() - i);
+            st.push(i);
+        }
+        for(int i = 0; i<n; i++){
+            sum = (sum + (long long) arr[i]*left[i]*right[i])% mod;
+        }
+        return (int)sum;
+    }
+};
