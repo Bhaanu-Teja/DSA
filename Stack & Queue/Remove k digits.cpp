12e7251625ca03fc58b440d909724c3fb1a3a3fb
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-26 12:10:49 +0530

    Add solution for removing k digits from a number
    Implement a solution to remove k digits from a number to minimize its value.

---
+++ Stack & Queue/Remove k digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n = num.size();
+        stack<char>st;
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && k > 0 && st.top() > num[i] ){
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        while(k > 0){
+            st.pop();
+            k--;
+        }
+        if(st.empty()) return "0";
+        string res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+
+        int i = 0;
+        while(i <(int)res.size() && res[i] == '0') i++;
+        res = res.substr(i);
+        return res.empty() ? "0" : res;
+    }
+};
