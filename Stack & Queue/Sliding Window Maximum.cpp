269ea7116d03ab4fe389123be0f3a673f0d34b3c
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-26 22:44:43 +0530

    Implement optimal sliding window maximum solution
    Added an optimal solution for the sliding window maximum problem using a deque.

--- Stack & Queue/Sliding Window Maximum.cpp
+++ Stack & Queue/Sliding Window Maximum.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -11,5 +12,25 @@ public:
             maxii.push_back(maxi);
         }
         return maxii;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int>dq;
+        vector<int>ans;
+        for(int i = 0; i<n; i++){
+            if(!dq.empty() && dq.front() <= i-k)
+                dq.pop_front();
+            while(!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if(i >= k-1) 
+                ans.push_back(nums[dq.front()]);
+        }
+        return ans;
     }
 };
