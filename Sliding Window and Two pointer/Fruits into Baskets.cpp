85577aae768cff9619a52604c97438a87b597cd6
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-30 17:30:21 +0530

    Implement optimal solution for fruit basket problem
    Added an optimal solution using the sliding window technique to calculate the maximum number of fruits in two baskets.

--- Sliding Window and Two pointer/Fruits into Baskets.cpp
+++ Sliding Window and Two pointer/Fruits into Baskets.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution{
   public:
     int totalFruits(vector<int>& fruits){
@@ -12,6 +13,28 @@ class Solution{
                 }
                 else break;
             }
+        }
+        return maxLen;
+    }
+};
+
+// Optimal
+class Solution{
+  public:
+    int totalFruits(vector<int>& fruits){
+        int n = fruits.size();
+        int l = 0, r = 0, maxLen = 0;
+        int k = 2;
+        map<int,int>mpp;
+        while(r < n){
+            mpp[fruits[r]]++;
+            if(mpp.size() > k){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
+                l++;
+            }
+            if(mpp.size() <= k) maxLen = max(maxLen, r-l+1);
+            r++;
         }
         return maxLen;
     }
