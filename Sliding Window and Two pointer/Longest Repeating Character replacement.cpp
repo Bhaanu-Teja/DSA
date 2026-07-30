cbccbe9299adc4d1590da879b76933705b8d17d6
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-30 22:44:20 +0530

    Update Longest Repeating Character replacement.cpp

--- Sliding Window and Two pointer/Longest Repeating Character replacement.cpp
+++ Sliding Window and Two pointer/Longest Repeating Character replacement.cpp
@@ -18,3 +18,23 @@ public:
         return maxLen;
     }
 };
+
+// Optimal
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int l = 0, r = 0, maxLen = 0, maxFreq = 0;
+        int hash[26] = {0};
+        for(int r = 0; r< s.size(); r++){
+            hash[s[r] - 'A']++;
+            maxFreq = max(maxFreq, hash[s[r] - 'A']);
+            while((r-l+1) - maxFreq > k){
+                hash[s[l] - 'A']--;
+                l++;
+            }
+            maxLen = max(maxLen, r-l+1);
+        }
+        return maxLen;
+    }
+};
