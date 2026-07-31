8b1b3e650fc516c60f21563eee835632918a79ef
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-07-31 15:07:48 +0530

    Add optimal solution for Binary Subarrays with Sum

--- Sliding Window and Two pointer/Binary Subarrays with Sum.cpp
+++ Sliding Window and Two pointer/Binary Subarrays with Sum.cpp
@@ -16,3 +16,28 @@ public:
         return ans;
     }
 };
+
+// Optimal
+class Solution {
+public:
+
+    int atMostSum(vector<int>& nums, int goal){
+        if(goal < 0) return 0;
+        int n = nums.size();
+        int l = 0, r = 0, sum = 0, ans = 0;
+        while(r < n){
+            sum += nums[r];
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
+            }
+            ans += r - l + 1;
+            r++;
+        }
+        return ans;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMostSum(nums, goal) - atMostSum(nums, goal-1);
+    }
+};
