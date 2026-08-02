350eb21df1afd346c00a30af3d0d5d7e2f3ad497
Bhaanu Teja <bhaanuteja999@gmail.com>
2026-08-02 19:46:50 +0530

    Implement optimal anagram check using counting
    Added an optimal solution for checking anagrams using character counting.

--- Others/Valid Anagram.cpp
+++ Others/Valid Anagram.cpp
@@ -1,3 +1,4 @@
+// Brute
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -5,5 +6,20 @@ public:
         sort(t.begin(), t.end());
         if(s == t) return true;
         else return false;
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        int count[26] = {0};
+        for(char c : s) count[c - 'a']++;
+        for(char c : t) count[c - 'a']--;
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
     }
 };
